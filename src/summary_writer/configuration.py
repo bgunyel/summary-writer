6f2e0dd0f6bf2ab6dea8bc2fb28d0172dcb33aaa
Bertan Günyel <bertan.gunyel@gmail.com>
2025-06-23 12:15:36 +0300

    Configuration class modified

--- src/summary_writer/configuration.py
+++ src/summary_writer/configuration.py
@@ -1,16 +1,9 @@
-import os
-import importlib
-
-from pydantic import BaseModel
-from langchain_core.runnables import RunnableConfig
 from ai_common import CfgBase, TavilySearchCategory
 
 
 class Configuration(CfgBase):
     """The configurable fields for the workflow"""
-    # thread_id: str
     max_iterations: int
-    # max_retries: int # in case LLM call fails, the number of retries
     max_results_per_query: int
     max_tokens_per_source: int
     number_of_days_back: int = None
